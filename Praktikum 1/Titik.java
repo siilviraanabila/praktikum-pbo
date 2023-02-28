74b430bf3ade49ecfcdf28232f82a113fb400662
siilviraanabila <100180250+siilviraanabila@users.noreply.github.com>
2023-03-01 00:20:45 +0700

    Update Titik.java

--- Praktikum 1/Titik.java
+++ Praktikum 1/Titik.java
@@ -1,1 +1,44 @@
+/**
+* Titik.java 22/02/2023
+* Penulis : Silvira Nabila Anggita Giraldi
+* Deskripsi : overload konstruktor pada kelas titik.
+*
+*
+*/
+class Titik{
+    double absis;
+    double ordinat;
+    static int counterTitik;
 
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik++;
+    }
+
+    Titik(double a, double o){
+        absis = a;
+        ordinat = o;
+        counterTitik++;
+    }
+
+    void setAbsis(double a){
+        absis =a;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    void setOrdinat(double o){
+        ordinat = o;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    int getCounterTitik(){
+        return counterTitik;
+    }
+}
