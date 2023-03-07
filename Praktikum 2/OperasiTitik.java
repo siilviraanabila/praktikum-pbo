4e592d3fcc65fb5b264946160565031c7716ac6c
siilviraanabila <100180250+siilviraanabila@users.noreply.github.com>
2023-03-07 22:42:11 +0700

    Update OperasiTitik.java

--- Praktikum 2/OperasiTitik.java
+++ Praktikum 2/OperasiTitik.java
@@ -1,3 +1,10 @@
+/**
+Pembuat : Silvira Nabila Anggita Giraldi
+NIM : 24060121120011
+Tanggal : 1 Maret 2023
+Deskripsi : Program yang berisi operasi titik.
+Lab : B1
+**/
 class OperasiTitik {
 	public void refleksiSumbuX(Titik titik) {
 		Double ordinat = titik.getOrdinat(); 
