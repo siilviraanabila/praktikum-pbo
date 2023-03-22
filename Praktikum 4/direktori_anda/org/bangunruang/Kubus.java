d2a0e7ee927dab59b0ae53ec3b0b56eb47858ba1
siilviraanabila <100180250+siilviraanabila@users.noreply.github.com>
2023-03-22 14:17:40 +0700

    Update Kubus.java

--- Praktikum 4/direktori_anda/org/bangunruang/Kubus.java
+++ Praktikum 4/direktori_anda/org/bangunruang/Kubus.java
@@ -8,23 +8,21 @@ Lab		: B1
 */
 
 package org.bangunruang;
-
-import org.bangundatar.PersegiPanjang;
-
-public class Kubus{
-    private PersegiPanjang permukaan;
-
-    public Kubus(PersegiPanjang permukaan){
-        this.permukaan = permukaan;
-    }
-
-    public double hitungVolume(){
-        double panjangSisi = permukaan.getPanjangSisi();
-        return panjangSisi * panjangSisi * panjangSisi;
-    }
-
-    public double hitungLuasAlas(){
-        double panjangSisi = permukaan.getPanjangSisi();
-        return panjangSisi * panjangSisi;
-    }
+  
+  import org.bangundatar.BujurSangkar;
+  
+  public class Kubus{
+	private BujurSangkar permukaan;
+	
+	public Kubus(BujurSangkar permukaan){
+		this.permukaan = permukaan;
+	}
+	public double hitungVolume(){
+		double panjangSisi = permukaan.getPanjangSisi();
+		return panjangSisi * panjangSisi * panjangSisi;
 }
+	public double hitungLuasAlas(){
+		double luasAlas = permukaan.hitungLuas();
+		return luasAlas;
+}
+  }
